ca8ea2b4d7d527cf3f1c4900706106b9a830f80e
eric <eric.ou@yale.edu>
2020-07-01 10:08:02 -0700

    checkpoint before creating likes

--- App/config/routes.rb
+++ App/config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
+  resources :likes
+  post '/logout', to: 'sessions#destroy'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/home', to: 'home#display'
+  root :to =>'sessions#welcome'
   resources :comments
   resources :post_tags
   resources :tags
   resources :posts
   resources :users
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  get '/home', to: 'home#display'
-  post '/logout', to: 'sessions#destroy'
-  root :to =>'sessions#welcome'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
