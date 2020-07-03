72df34c2ba5e89076927ce4fab72b4db86df8458
eric <eric.ou@yale.edu>
2020-07-02 21:34:40 -0700

    Everything Works V2

--- App/app/controllers/users_controller.rb
+++ App/app/controllers/users_controller.rb
@@ -36,7 +36,7 @@ class UsersController < ApplicationController
     private
 
     def user_params 
-        params.require(:user).permit(:user_name, :password, :password_confirmation)
+        params.require(:user).permit(:user_name, :password, :password_confirmation, :image)
     end
 
     def validate(error_path)
